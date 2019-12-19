055f5715a3c2629efa636e2be9a81fc8e1042eba
pang9988 <pzy5021@163.com>
2019-12-19 23:08:42 +0800

    66

---
+++ douyin/miniprogram/pages/my1/my1.js
@@ -0,0 +1,84 @@
+// pages/my1/my1.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  // 安全须知
+  aq(){
+    wx.navigateTo({
+      url: '/pages/anquan/anquan',
+    })
+  },
+  // 申请成为舞蹈教练
+  shenqi(){
+    wx.navigateTo({
+      url: '/pages/sq/sq',
+    })
+  },
+  // 加盟商注册
+  jiameng(){
+    wx.navigateTo({
+      url: '/pages/zc/zc',
+    })
+  }
+})
