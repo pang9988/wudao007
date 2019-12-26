626b97d16828fcc728b6090a6897332ef0a33091
pang9988 <pzy5021@163.com>
2019-12-27 01:03:56 +0800

    99

--- douyin/miniprogram/pages/sqzl/sqzl.js
+++ douyin/miniprogram/pages/sqzl/sqzl.js
@@ -5,106 +5,68 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 删除图片
+    indextu:[],
+     // 目前居住城市假数据,
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部',
+    multiIndex: [0, 0, 0],
     // 评级假数据,
     array: ['AAA', 'BBB', 'CCC', 'DDD'],
     objectArray: [
-      {
-        id: 0,
-        name: 'AAA'
-      },
-      {
-        id: 1,
-        name: 'BBB'
-      },
-      {
-        id: 2,
-        name: 'CCC'
-      },
-      {
-        id: 3,
-        name: 'DDD'
-      }
+      {id: 0,name: 'AAA'},
+      {id: 1,name: 'BBB'},
+      {id: 2,name: 'CCC'},
+      {id: 3,name: 'DDD'}
     ],
     // 身高
     // 身高假数据,
     array1: ['155~165', '165~175', '175~180', '180以上'],
     objectArray: [
-      {
-        id: 0,
-        name: '155~165'
-      },
-      {
-        id: 1,
-        name: '165~175'
-      },
-      {
-        id: 2,
-        name: '175~180'
-      },
-      {
-        id: 3,
-        name: '180以上'
-      }
+      {id: 0, name: '155~165'},
+      {id: 1,name: '165~175'},
+      {id: 2,name: '175~180'},
+      {id: 3,name: '180以上'}
     ],
     // 性别假数据,
     array2: ['男', '女', '富婆', '富公'],
     objectArray: [
-      {
-        id: 0,
-        name: '男'
-      },
-      {
-        id: 1,
-        name: '女'
-      },
-      {
-        id: 2,
-        name: '富婆'
-      },
-      {
-        id: 3,
-        name: '富公'
-      }
+      {id: 0,name: '男'},
+      {id: 1,name: '女'},
+      {id: 2,name: '富婆'},
+      {id: 3,name: '富公'}
     ],
-    // 身高假数据,
-    array3: ['155~165', '165~175', '175~180', '180以上'],
+    // 属相假数据,
+    array3: ['蛇', '龙', '虎', '鼠'],
     objectArray: [
-      {
-        id: 0,
-        name: '155~165'
-      },
-      {
-        id: 1,
-        name: '165~175'
-      },
-      {
-        id: 2,
-        name: '175~180'
-      },
-      {
-        id: 3,
-        name: '180以上'
-      }
+      { id: 0, name: '蛇'},
+      { id: 1, name: '龙'},
+      { id: 2, name: '虎'},
+      { id: 3, name: '鼠'}
     ],
-    // 身高假数据,
-    array4: ['155~165', '165~175', '175~180', '180以上'],
+    // 星座假数据,
+    array4: ['獅子座', '射手座', '水瓶座', '天秤座'],
     objectArray: [
-      {
-        id: 0,
-        name: '155~165'
-      },
-      {
-        id: 1,
-        name: '165~175'
-      },
-      {
-        id: 2,
-        name: '175~180'
-      },
-      {
-        id: 3,
-        name: '180以上'
-      }
+      { id: 0, name: '獅子座'},
+      { id: 1, name: '射手座'},
+      { id: 2, name: '水瓶座'},
+      { id: 3, name: '天秤座'}
+    ],
+    // 年龄范围假数据,
+    array5: ['18~22', '22~28', '28~35', '35~45'],
+    objectArray: [
+      { id: 0, name: '18~22'},
+      { id: 1, name: '22~28'},
+      { id: 2, name: '28~35'},
+      { id: 3, name: '35~45'}
+    ],
+    // 血型假数据,
+    array6: ['A', 'B', 'AB', 'O'],
+    objectArray: [
+      { id: 0, name: 'A'},
+      { id: 1, name: 'B'},
+      { id: 2, name: 'AB'},
+      { id: 3, name: 'O'}
     ],
     isshow:true,
     fileList: [
@@ -182,6 +144,27 @@ Page({
       index4: e.detail.value
     })
   },
+  // 年龄范围
+  bindPickerChange5: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index5: e.detail.value
+    })
+  },
+  // 血型
+  bindPickerChange6: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index6: e.detail.value
+    })
+  },
+  // 目前居住城市
+  bindRegionChange7: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -242,6 +225,12 @@ Page({
   myfs(){
     wx.navigateBack({
       
+    })
+  },
+  // 删除
+  shanchu(e){
+    this.setData({
+      indextu: e.detail.index
     })
   }
 })
